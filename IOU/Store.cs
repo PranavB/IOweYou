b7d593fb672c108703a978ee72cee3cdbfdb386f
Pranav Bolar <pranavbolar@outlook.com>
2014-10-06 11:29:23 +0530

    Initial commit
    Added all files in second release to Windows Store

---
+++ IOU/Store.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization.Json;
+using IOU.Commands;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Text;
+using Windows.Globalization.NumberFormatting;
+using Windows.System.UserProfile;
+using Windows.Globalization;
+
+namespace IOU
+{
+    public class Store : INotifyPropertyChanged
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double amount { get; set; }
+        public string currency { get; set; }
+
+        [IgnoreDataMember]
+        public ICommand deleteCommand { get; set; }
+
+        public Store()
+        {
+            deleteCommand = new deleteButtonClick();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+
+    // ------------------------------------------------------------------------------------------------------
+
+
+    public class StoreDataSource
+    {
+        const string storageFile = "ioufile.json";
+        private ObservableCollection<Store> _debts;
+
+        public StoreDataSource()
+        {
+            _debts = new ObservableCollection<Store>();
+        }
+
+        public async Task initialLoad()
+        {
+            await readJSON();
+            if (_debts == null)
+                _debts = new ObservableCollection<Store>();
+        }
+
+        private async Task readJSON()
+        {
+            if (_debts == null) return;
+            var jsonSerializer = new DataContractJsonSerializer(typeof(ObservableCollection<Store>));
+
+            try
+            {
+                using (var stream = await ApplicationData.Current.LocalFolder.OpenStreamForReadAsync(storageFile))
+                {
+                    _debts = (ObservableCollection<Store>)jsonSerializer.ReadObject(stream);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Whoops: {0}", e);
+            }
+        }
+
+        public async void addToOCandStore(string n, double a)
+        {
+            double amt = a;
+            
+            // look for existing debt
+            foreach (Store item in _debts)
+            {
+                if (n == item.name)
+                {
+                    amt += item.amount;
+                    _debts.Remove(item);
+                    break;
+                }
+            }
+
+            Store st = new Store();
+            if (_debts.Count == 0)
+                st.id = 0;
+            else
+                st.id = _debts.Last().id + 1;
+            st.name = n;
+            st.amount = amt;
+
+            string currency = GlobalizationPreferences.Currencies[0];
+            StringBuilder amountAsCurrency = new StringBuilder();
+            CurrencyFormatter defaultCurrencyFormatter = new CurrencyFormatter(currency);
+            defaultCurrencyFormatter.IsGrouped = true;
+            amountAsCurrency.AppendLine(defaultCurrencyFormatter.Format(st.amount));
+            st.currency = amountAsCurrency.ToString();
+
+            _debts.Add(st);
+
+            await storeJSON();
+        }
+
+        private async Task storeJSON()
+        {
+            var jsonSerializer = new DataContractJsonSerializer(typeof(ObservableCollection<Store>));
+            try
+            {
+                using (var stream = await ApplicationData.Current.LocalFolder.OpenStreamForWriteAsync(storageFile,
+                    CreationCollisionOption.ReplaceExisting))
+                {
+                    jsonSerializer.WriteObject(stream, _debts);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Whoops becauase: {0}", e);
+            }
+        }
+
+        public async void deleteDebt(Store s)
+        {
+            //_debts.Remove(s);
+            short i = 0, flag = 0;
+            foreach (Store item in _debts)
+            {
+                if (item.id == s.id)
+                {
+                    flag = 1;
+                    break;
+                }
+                i++;
+            }
+            if (flag == 1)
+            {
+                _debts.RemoveAt(i);
+            }
+            await storeJSON();
+        }
+
+        public ObservableCollection<Store> getObservableItems()
+        {
+            return _debts;
+        }
+
+    }
+
+    //-----------------------------------------------------------------------------------------------------
+
+    public class deleteButtonClick : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            App.sds.deleteDebt((Store)parameter);
+        }
+    }
+
+}
---
+++ IOU/addDebt.xaml.cs
@@ -0,0 +1,199 @@
+﻿using IOU.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Contacts;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Phone.UI.Input;
+using Windows.Storage.Streams;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace IOU
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class addDebt : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        Store st;
+
+        public addDebt()
+        {
+            this.InitializeComponent();
+
+            st = new Store();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// Gets the view model for this <see cref="Page"/>.
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+            
+        }
+
+        #region NavigationHelper registration
+
+        /// <summary>
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// <para>
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="NavigationHelper.LoadState"/>
+        /// and <see cref="NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+        /// </para>
+        /// </summary>
+        /// <param name="e">Provides data for navigation methods and event
+        /// handlers that cannot cancel the navigation request.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+            
+        }
+
+        #endregion
+
+        private async void PickAContactBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var contactPicker = new Windows.ApplicationModel.Contacts.ContactPicker();
+            contactPicker.DesiredFieldsWithContactFieldType.Add(ContactFieldType.PhoneNumber);
+            Contact contact = await contactPicker.PickContactAsync();
+
+            if (contact != null)
+            {
+                nameTextBlock.Text = contact.DisplayName;
+
+                if (contact.Thumbnail != null)
+                {
+                    IRandomAccessStreamWithContentType stream = await contact.Thumbnail.OpenReadAsync();
+                    if (stream != null && stream.Size > 0)
+                    {
+                        BitmapImage bitmap = new BitmapImage();
+                        bitmap.SetSource(stream);
+                        contactThumbnail.Source = bitmap;
+                        contactThumbnail.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        contactThumbnail.Source = null;
+                    }
+                }
+
+                saveBtn.Visibility = Visibility.Visible;
+                nameTextBlock.Visibility = Visibility.Visible;
+                amountTextBox.Visibility = Visibility.Visible;
+                amountHelpText.Visibility = Visibility.Visible;
+                PickAContactBtn.Visibility = Visibility.Collapsed;
+               
+            }
+            else
+            {
+                //whoops!
+            }            
+        }
+        
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            double amt;
+
+            if (!String.IsNullOrEmpty(amountTextBox.Text) && Double.TryParse(amountTextBox.Text, out amt))
+            {
+                App.sds.addToOCandStore(nameTextBlock.Text, amt);
+
+                if (Frame.CanGoBack)
+                    Frame.GoBack();
+                else
+                    Frame.Navigate(typeof(landing));
+            }
+            else
+            {
+                amountTextBox.Text = String.Empty;
+                Storyboard s = new Storyboard();
+                DoubleAnimation da1 = new DoubleAnimation();
+
+                da1.From = 1.0;
+                da1.To = 0.25;
+                da1.AutoReverse = true;
+                da1.Duration = new Duration(TimeSpan.FromMilliseconds(200));
+
+                Storyboard.SetTarget(da1, amountTextBox);
+                Storyboard.SetTargetProperty(da1, "Opacity");
+
+                s.Children.Add(da1);
+                s.Begin();
+                amountTextBox.Focus(Windows.UI.Xaml.FocusState.Programmatic); 
+            }
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+                Frame.GoBack();
+        }
+
+    }
+}
